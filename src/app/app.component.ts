acea9b0f254c838903203c8cfb21cb2bf1e4bb9b
LUALROAL <alejorodryguez@gmail.com>
2025-02-26 20:05:14 -0500

    creacin banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./shared/components/nav-bar/nav-bar.component";
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
